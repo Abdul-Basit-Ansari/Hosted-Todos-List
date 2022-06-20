9b63452c0456e9cff74602f80882498b7a578083
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-20 20:53:57 +0500

    Change root directory

--- todo/settings.py
+++ todo/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 from django.contrib import messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
