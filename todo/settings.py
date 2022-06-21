140e04c58702c5e815a124fe523cb077b8a6fb82
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-21 10:21:12 +0500

    Edit static url

--- todo/settings.py
+++ todo/settings.py
@@ -132,11 +132,11 @@ STATIC_URL = '/static/'
 
 if DEBUG:
 
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 else:
 
-  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
